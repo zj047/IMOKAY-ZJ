a4178668b9d9c870a3a2798d20752ad65c50ab7a
zj047 <109504133+zj047@users.noreply.github.com>
2022-10-01 20:32:34 +0800

    Add files via upload

---
+++ src/settings.7afb0.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/01-shouye/shouye.fire",orientation:"",jsList:[],bundleVers:{internal:"1b082",main:"bff02"}};
